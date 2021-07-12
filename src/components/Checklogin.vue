270c840d2332a69390f83f4e35dba4c0a68ad6c4
IRISGROUP Company <it@iris.co.th>
2021-07-12 10:52:26 +0700

    commit

---
+++ src/components/Checklogin.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Checklogin</div>
+</template>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,61 @@
 <template>
-    <div></div>
+  <div class="hold-transition login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <a href="/"><b>Admin</b>LTE</a>
+      </div>
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Sign in to start your session</p>
+
+          <form action="/checklogin" method="post">
+            <div class="input-group mb-3">
+              <input type="email" class="form-control" placeholder="Email" />
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-envelope"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Password"
+              />
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-8">
+                <div class="icheck-primary">
+ 
+                </div>
+              </div>
+              <div class="col-4">
+                <button type="submit" class="btn btn-primary btn-block">
+                  Sign In
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <div class="social-auth-links text-center mb-3">
+
+          </div>
+          <p class="mb-1">
+            <a href="/forgot_password">I forgot my password</a>
+          </p>
+          <p class="mb-0">
+            <a href="/register" class="text-center"
+              >Register a new membership</a
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
